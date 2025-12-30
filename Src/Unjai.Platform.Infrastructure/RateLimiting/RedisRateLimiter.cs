dd7ba539c9a518a5830d1225629f3aefbc158e8d
Ratchanon Unnu <ratchanon.u@smartmedtech.co.th>
2025-12-30 14:29:06 +0700

    feat: rate limit use lua script

--- Src/Unjai.Platform.Infrastructure/RateLimiting/RedisRateLimiter.cs
+++ Src/Unjai.Platform.Infrastructure/RateLimiting/RedisRateLimiter.cs
@@ -6,17 +6,23 @@ internal sealed class RedisRateLimiter(IConnectionMultiplexer multiplexer)
 {
     private readonly IDatabase _redis = multiplexer.GetDatabase();
 
+    private const string Script = """
+    local current = redis.call("INCR", KEYS[1])
+    if current == 1 then
+        redis.call("EXPIRE", KEYS[1], ARGV[1])
+    end
+    return current
+    """;
+
     public async Task<bool> IsAllowedAsync(
         string key,
         int limit,
         TimeSpan window)
     {
-        var count = await _redis.StringIncrementAsync(key);
-
-        if (count == 1)
-        {
-            await _redis.KeyExpireAsync(key, window);
-        }
+        var count = (long)await _redis.ScriptEvaluateAsync(
+            Script,
+            keys: [key],
+            values: [(int)window.TotalSeconds]);
 
         return count <= limit;
     }
