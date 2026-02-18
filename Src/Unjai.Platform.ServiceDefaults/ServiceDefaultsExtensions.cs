d05ca3e516bc20f6b7574039e11c1c8460ad0b9d
Ratchanon Unnu <ratchanon.u@smartmedtech.co.th>
2026-02-18 09:21:44 +0700

    feat: change http.AddStandardResilienceHandler

--- Src/Unjai.Platform.ServiceDefaults/ServiceDefaultsExtensions.cs
+++ Src/Unjai.Platform.ServiceDefaults/ServiceDefaultsExtensions.cs
@@ -33,7 +33,13 @@ public static class ServiceDefaultsExtensions
         builder.Services.ConfigureHttpClientDefaults(http =>
         {
             // Turn on resilience by default
-            http.AddStandardResilienceHandler();
+            http.AddStandardResilienceHandler(options =>
+            {
+                options.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(60);
+                options.AttemptTimeout.Timeout = TimeSpan.FromSeconds(30);
+                options.Retry.MaxRetryAttempts = 3;
+                options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(90);
+            });
 
             // Turn on service discovery by default
             http.AddServiceDiscovery();
